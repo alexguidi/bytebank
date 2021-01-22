2ffb2528969ef0ce913195df2db5b6b17bf050a1
Alex Guidi <aguidi@tfd.ie>
2021-01-22 23:03:08 +0000

    Adicionando formulario de transferencia

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,14 +1,28 @@
 import 'package:bytebank/models/saldo.dart';
 import 'package:bytebank/screens/dashboard/saldo.dart';
+import 'package:bytebank/screens/deposito/formulario.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class Dashboard extends StatelessWidget{
-  Widget build(BuildContext context){
+class Dashboard extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bytebank'),
-      ),
-      body: Align(alignment: Alignment.topCenter, child: SaldoCard()),
-    );
+        appBar: AppBar(
+          title: const Text('Bytebank'),
+        ),
+        body: ListView(children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: SaldoCard(),
+          ),
+          RaisedButton(
+              child: Text('Receber Deposito'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context){
+                  return FormularioDeposito();
+                }));
+              },
+            ),
+        ]));
   }
 }
